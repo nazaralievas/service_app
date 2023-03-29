9d2a38ed791d4c549056bbaa0f99a952c5338798
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-03-29 21:14:38 +0600

    calculating price with discount using SerializerMethodField

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import CharField
+from rest_framework.serializers import CharField, SerializerMethodField
 
-from .models import Subscription
+from .models import Subscription, Plan
+
+
+class PlanSerializer(ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ('__all__')
 
 
 class SubscriptionSerializer(ModelSerializer):
+    plan = PlanSerializer()
     client_name = CharField(source='client.company_name')
     email = CharField(source='client.user.email')
+    price = SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price - 
+                instance.service.full_price * (instance.plan.discount_percent / 100))
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
--- service/services/views.py
+++ service/services/views.py
@@ -8,6 +8,7 @@ from .serializers import SubscriptionSerializer
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    )
+        )
     serializer_class = SubscriptionSerializer
